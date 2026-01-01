00c32ecb669a9016ca334a0c80d2b97436bea2d8
Srikar@82006 <srikarmach@gmail.com>
2026-01-01 10:41:38 +0530

    Added wsgi

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,13 @@
 from app import create_app
 from flask_cors import CORS
+import os
 
 app = create_app()
-
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
+    )
